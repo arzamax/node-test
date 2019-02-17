87d9cd35ccab73c3c59018b236ae7dfadf6d3f49
arzamax <arzamax@gmail.com>
2019-02-17 20:20:05 +0400

    refactored html-server

--- server/http-servers/html-server.js
+++ server/http-servers/html-server.js
@@ -3,21 +3,25 @@ import url from 'url';
 import fs from 'fs';
 import path from 'path';
 import hbs from 'hbs';
+import readline from 'readline';
 
 const server = http.createServer((req, res) => {
-  fs.readFile(path.join(__dirname, 'data', 'index.html'), 'utf-8', (err, file) => {
-    if (err) {
-      console.log(err);
-
-    } else {
-      const template = hbs.compile(file);
-      const q = url.parse(req.url, true).query;
-      const context = { message: q.message || 'Some string' };
-
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-      res.end(template(context));
-    }
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(path.join(__dirname, 'data', 'index.html')),
   });
+  const q = url.parse(req.url, true).query;
+  const context = { message: q.message || 'Some string' };
+
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  lineReader
+    .on('line', line => {
+      const template = hbs.compile(line);
+
+      res.write(template(context));
+    })
+    .on('close', () => {
+      res.end();
+    })
 });
 
 server.listen(1337, err => {
